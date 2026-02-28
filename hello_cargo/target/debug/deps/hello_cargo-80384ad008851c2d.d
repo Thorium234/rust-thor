a9d01cc9ee41427d0f8663a942de56e7376c47bd
Thorium234 <inyangalapaul5@gmail.com>
2026-02-28 16:52:21 +0300

    added read me

---
+++ hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d.d
@@ -0,0 +1,5 @@
+/home/thorium/Desktop/programming/2026/rust/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d.d: src/main.rs
+
+/home/thorium/Desktop/programming/2026/rust/hello_cargo/target/debug/deps/hello_cargo-80384ad008851c2d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/thorium/Desktop/programming/2026/rust/hello_cargo/target/debug/hello_cargo: /home/thorium/Desktop/programming/2026/rust/hello_cargo/src/main.rs
