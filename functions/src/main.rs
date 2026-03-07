050ac5274c6fbdff863af3ca524011539e6ced60
Thorium234 <inyangalapaul5@gmail.com>
2026-03-07 22:36:39 +0300

    Compiling functions v0.1.0 (/home/thorium/Desktop/programming/2026/Rust/milestone1/functions) error[E0308]: mismatched types  --> src/main.rs:6:32   | 6 |     human_details(thorium,55,182);   |     -------------              ^^^ expected , found integer   |     |   |     arguments to this function are incorrect   | note: function defined here  --> src/main.rs:9:4   | 9 | fn human_details(name:&str,age:u32,height:f32){   |    ^^^^^^^^^^^^^                   ---------- help: use a float literal   | 6 |     human_details(thorium,55,182.0);   |                                   ++
    For more information about this error, try Expected type did not match the received type.
    
    Erroneous code examples:
    
    ```
    fn plus_one(x: i32) -> i32 {
        x + 1
    }
    
    plus_one("Not a number");
    //       ^^^^^^^^^^^^^^ expected `i32`, found `&str`
    
    if "Not a bool" {
    // ^^^^^^^^^^^^ expected `bool`, found `&str`
    }
    
    let x: f32 = "Not a float";
    //     ---   ^^^^^^^^^^^^^ expected `f32`, found `&str`
    //     |
    //     expected due to this
    ```
    
    This error occurs when an expression was used in a place where the compiler
    expected an expression of a different type. It can occur in several cases, the
    most common being when calling a function and passing an argument which has a
    different type than the matching type in the function declaration..
    error: could not compile  (bin functions) due to 1 previous error

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,23 @@
 fn main() {
     println!("Hello, world!");
+
     hello_world();
+    tell_height(182);
+    human_details("thorium",55,182.0);
+}
+
+fn human_details(name:&str,age:u32,height:f32){
+
+    println!("my name is {}, im {}years old and my height is {} cm.",name,age,height);
 }
 
 fn hello_world(){
  println!("hello, Rust!");
+}
+
+//hoisting-where you define and call function
+//
+//inserting value 
+fn tell_height(height:u32){
+ println!("my height is {}",height);
 }
