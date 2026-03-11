84a1c2a2bb3887e27970d18229bc814f308927a3
Thorium234 <inyangalapaul5@gmail.com>
2026-03-11 22:09:10 +0300

    taking user input

--- example4/src/main.rs
+++ example4/src/main.rs
@@ -2,9 +2,15 @@ use std::io;
 fn main() {
     println!("Greetings app");
     println!("============="); 
-    println!("please enter your name:");
-    let mut name = String::new();
+    println!("please enter your first  name:");
+    //println!("please enter your second name:");
+    let mut fname = String::new();
+   // let mut lname = String:: new();
     let _= io:: stdin()
-        .read_line(&mut name);
-     println!("Hello,{name} hope you are doing well");   
+        .read_line(&mut fname);
+    println!("please enter your second name: ");
+    let mut lname = String:: new();
+    let _= io::stdin()
+        .read_line(&mut lname);
+     println!("Hello,{fname}{lname} hope you are doing well");   
 }
