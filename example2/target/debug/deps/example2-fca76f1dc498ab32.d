b4f84eafdba162c58f6be0a3e31704f1d23222eb
Thorium234 <inyangalapaul5@gmail.com>
2026-03-11 14:25:37 +0300

    added example 2 of bmi calculation

---
+++ example2/target/debug/deps/example2-fca76f1dc498ab32.d
@@ -0,0 +1,5 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/example2/target/debug/deps/example2-fca76f1dc498ab32.d: src/main.rs
+
+/home/thorium/Desktop/programming/2026/Rust/milestone1/example2/target/debug/deps/example2-fca76f1dc498ab32: src/main.rs
+
+src/main.rs:
---
+++ example2/target/debug/example2.d
@@ -0,0 +1,1 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/example2/target/debug/example2: /home/thorium/Desktop/programming/2026/Rust/milestone1/example2/src/main.rs
