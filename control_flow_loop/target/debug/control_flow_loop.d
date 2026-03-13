149c9674b68df0b5c688a7c829cca6d61fd23dc3
Thorium234 <inyangalapaul5@gmail.com>
2026-03-13 22:46:25 +0300

    The loop keyword tells Rust to execute a block of code over and over again either forever or until you explicitly tell it to stop.

---
+++ control_flow_loop/target/debug/control_flow_loop.d
@@ -0,0 +1,1 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_loop/target/debug/control_flow_loop: /home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_loop/src/main.rs
---
+++ control_flow_loop/target/debug/deps/control_flow_loop-9625994202b4aa19.d
@@ -0,0 +1,5 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_loop/target/debug/deps/control_flow_loop-9625994202b4aa19.d: src/main.rs
+
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_loop/target/debug/deps/control_flow_loop-9625994202b4aa19: src/main.rs
+
+src/main.rs:
