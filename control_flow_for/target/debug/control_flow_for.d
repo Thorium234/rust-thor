0a0034f83f4b76e477db8fa309c33bfe25cb0258
Thorium234 <inyangalapaul5@gmail.com>
2026-03-14 23:13:53 +0300

    As a more concise alternative, you can use a for loop and execute some code for each item in a collection. A for loop looks like the code in Listing 3-5

---
+++ control_flow_for/target/debug/control_flow_for.d
@@ -0,0 +1,1 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_for/target/debug/control_flow_for: /home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_for/src/main.rs
---
+++ control_flow_for/target/debug/deps/control_flow_for-34035fc7d66fada3.d
@@ -0,0 +1,5 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_for/target/debug/deps/control_flow_for-34035fc7d66fada3.d: src/main.rs
+
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_for/target/debug/deps/control_flow_for-34035fc7d66fada3: src/main.rs
+
+src/main.rs:
