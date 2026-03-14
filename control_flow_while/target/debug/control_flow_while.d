d79664d83935a744b86a74a1448467ba96112bfd
Thorium234 <inyangalapaul5@gmail.com>
2026-03-14 23:06:02 +0300

    A program will often need to evaluate a condition within a loop. While the condition is true , the loop runs. When the condition ceases to be true , the program calls break , stopping the loop. It’s possible to implement behavior like this using a combination of loop , if , else , and break ; you could try that now in a program, if you’d like. However, this pattern is so common that Rust has a built-in language construct for it, called a while loop. In Listing 3-3, we use while to loop the program three times, counting down each time, and then, after the loop, to print a message and exit.

---
+++ control_flow_while/target/debug/control_flow_while.d
@@ -0,0 +1,1 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_while/target/debug/control_flow_while: /home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_while/src/main.rs
---
+++ control_flow_while/target/debug/deps/control_flow_while-d02d3e3ed3d8aafa.d
@@ -0,0 +1,5 @@
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_while/target/debug/deps/control_flow_while-d02d3e3ed3d8aafa.d: src/main.rs
+
+/home/thorium/Desktop/programming/2026/Rust/milestone1/control_flow_while/target/debug/deps/control_flow_while-d02d3e3ed3d8aafa: src/main.rs
+
+src/main.rs:
