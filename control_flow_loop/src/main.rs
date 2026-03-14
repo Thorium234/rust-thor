dae94d795cb98a6a1ffe3fe589fd837e619df18e
Thorium234 <inyangalapaul5@gmail.com>
2026-03-14 23:02:16 +0300

    The outer loop has the label 'counting_up , and it will count up from 0 to 2. The inner loop without a label counts down from 10 to 9. The first break that doesn’t specify a label will exit the inner loop only. The break 'counting_up; statement will exit the outer loop.

--- control_flow_loop/src/main.rs
+++ control_flow_loop/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+/*fn main() {
 let mut counter = 0;
 let result = loop {
 counter += 1;
@@ -7,4 +7,24 @@ break counter * 2;
 }
 };
 println!("The result is {result}");
+}*/
+
+fn main() {
+let mut count = 0;
+'counting_up: loop {
+println!("count = {count}");
+let mut remaining = 10;
+loop {
+println!("remaining = {remaining}");
+if remaining == 9 {
+break;
+}
+if count == 2 {
+break 'counting_up;
+}
+remaining -= 1;
+}
+count += 1;
+}
+println!("End count = {count}");
 }
