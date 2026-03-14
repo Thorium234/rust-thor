15bd09c2bda7ef2bc066d4f5f9ed9c307f2b9b65
Thorium234 <inyangalapaul5@gmail.com>
2026-03-14 23:11:36 +0300

    You can choose to use the while construct to loop over the elements of a collection, such as an array. For example, the loop in Listing 3-4 prints each element in the array a .

--- control_flow_while/src/main.rs
+++ control_flow_while/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
+/*fn main() {
 let mut number = 3;
 while number != 0 {
 println!("{number}!");
 number -= 1;
 }
 println!("LIFTOFF!!!");
+}*/
+fn main() {
+let a = [10, 20, 30, 40, 50];
+let mut index = 0;
+while index < 5 {
+println!("the value is: {}", a[index]);
+index += 1;
 }
-
+}
